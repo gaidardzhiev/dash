gaidardzhiev/dash

commit 9881d00e939e75e5348aebe6046ff80d3b7edb17

jobs: Preserve parent jobs for simple commands
Do not free parent shell jobs if a simple command with the first
word being "jobs" is executed as a command substitution.

Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/jobs.c
@@ -53,6 +53,7 @@
 #include <termios.h>
 #undef CEOF			/* syntax.h redefines this */
 #endif
+#include "builtins.h"
 #include "exec.h"
 #include "eval.h"
 #include "init.h"
@@ -911,6 +912,11 @@ static void forkchild(struct job *jp, union node *n, int mode)
 	}
 
 	if (lvforked)
+		return;
+
+	freejob(jp);
+
+	if (issimplecmd(n, JOBSCMD->name))
 		return;
 
 	for (jp = curjob; jp; jp = jp->prev_job)

src/parser.c
@@ -133,6 +133,12 @@ int isassignment(const char *p)
 	return *q == '=';
 }
 
+int issimplecmd(union node *n, const char *name)
+{
+	return n && n->type == NCMD && n->ncmd.args &&
+	       equal(n->ncmd.args->narg.text, name);
+}
+
 static inline int realeofmark(const char *eofmark)
 {
 	return eofmark && eofmark != FAKEEOFMARK;

src/parser.h
@@ -36,6 +36,8 @@
 
 #include "token.h"
 
+union node;
+
 /* control characters in argument strings */
 #define CTL_FIRST -127		/* first 'special' character */
 #define CTLESC -127		/* escape next character */
@@ -85,6 +87,7 @@ extern int checkkwd;
 
 
 int isassignment(const char *p);
+int issimplecmd(union node *n, const char *name);
 union node *parsecmd(int);
 void fixredir(union node *, const char *, int);
 const char *getprompt(void *);

src/trap.c
@@ -37,6 +37,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "builtins.h"
 #include "shell.h"
 #include "main.h"
 #include "nodes.h"	/* for other headers */
@@ -47,6 +48,7 @@
 #include "options.h"
 #include "syntax.h"
 #include "output.h"
+#include "parser.h"
 #include "memalloc.h"
 #include "error.h"
 #include "trap.h"
@@ -170,8 +172,7 @@ void clear_traps(union node *n)
 	int simplecmd;
 	char **tp;
 
-	simplecmd = n && n->type == NCMD && n->ncmd.args &&
-		    equal(n->ncmd.args->narg.text, "trap");
+	simplecmd = issimplecmd(n, TRAPCMD->name);
 
 	INTOFF;
 	for (tp = trap ; tp < &trap[NSIG] ; tp++) {

commit 8347bea6fd658ae665582d6ea103d7cc1859e812

jobs: Fix here-document crash
When forkchild is called while expanding here-documents, the job
pointer is set to NULL.  If this is the case, there is no need
to free the job pointer and other outstanding jobs.  Simply return
if this is the case.

Reported-by: Hadrien LACOUR <h.lacour@sim-and-cure.com>
Fixes: 9881d00e939e ("jobs: Preserve parent jobs for simple commands")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/jobs.c
@@ -919,6 +919,9 @@ static void forkchild(struct job *jp, union node *n, int mode)
 	if (lvforked)
 		return;
 
+	if (!jp)
+		return;
+
 	freejob(jp);
 
 	if (issimplecmd(n, JOBSCMD->name))