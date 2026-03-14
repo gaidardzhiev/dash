gaidardzhiev/dash

commit 6abb589582c62cf64b61c6725af17e853561e3df

input: Call input_init on first use
When the shell is used to execute another utility, it makes no
sense to initialise stdin, which costs up to two system calls.

Since the input layer can already handle first-use initialisation
because of reset_init, simply make this always the case.

Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/input.c
@@ -77,7 +77,7 @@ MKINIT struct parsefile *toppf = &basepf;
 MKINIT struct stdin_state stdin_state;
 struct parsefile *parsefile = &basepf;	/* current input file */
 int whichprompt;		/* 1 == PS1, 2 == PS2 */
-int stdin_istty;
+int stdin_istty = -1;
 
 MKINIT void input_init(void);
 
@@ -99,8 +99,6 @@ INCLUDE "syntax.h"
 INIT {
 	basepf.nextc = basepf.buf = basebuf;
 	basepf.linno = 1;
-
-	input_init();
 }
 
 RESET {

commit 74adb5aaa63a857d5db678bd873fcd6a193da8d1

options: Call input_init before using stdin_istty
Call input_init before using stdin_istty as otherwise it will
always be true.

Reported-by: Juergen Daubert <j.daubert@posteo.de>
Fixes: 6abb589582c6 ("input: Call input_init on first use")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/input.c
@@ -79,8 +79,6 @@ struct parsefile *parsefile = &basepf;	/* current input file */
 int whichprompt;		/* 1 == PS1, 2 == PS2 */
 int stdin_istty = -1;
 
-MKINIT void input_init(void);
-
 STATIC void pushfile(void);
 static void popstring(void);
 static int preadfd(void);

src/input.h
@@ -119,6 +119,7 @@ void unwindfiles(struct parsefile *);
 void popallfiles(void);
 void flush_input(void);
 void reset_input(void);
+void input_init(void);
 
 static inline int input_get_lleft(struct parsefile *pf)
 {

src/options.c
@@ -138,8 +138,11 @@ procargs(char **xargv)
 			sh_error("-c requires an argument");
 		sflag = 1;
 	}
-	if (iflag == 2 && sflag == 1 && stdin_istty && isatty(2))
-		iflag = 1;
+	if (iflag == 2 && sflag == 1) {
+		input_init();
+		if (stdin_istty && isatty(2))
+			iflag = 1;
+	}
 	if (mflag == 2)
 		mflag = iflag;
 	for (i = 0; i < NOPTS; i++)