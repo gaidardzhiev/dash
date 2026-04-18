gaidardzhiev/dash

commit e8c0324af64c68f59d063ecfa473c0793d1c0c98

parser: Catch bogus case patterns
If the token in a case statement pattern is not a TWORD, do not
insert it into the pattern list.  Instead abort the parsing with
a syntax error.

Reported-by: Aleksander Ushakov <retes672@gmail.com>
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/parser.c
@@ -442,6 +442,8 @@ next_case:
 			cp->type = NCLIST;
 			app = &cp->nclist.pattern;
 			for (;;) {
+				if (lasttoken != TWORD)
+					synexpect(TWORD);
 				*app = ap = (union node *)stalloc(sizeof (struct narg));
 				ap->type = NARG;
 				ap->narg.text = wordtext;

commit 8341dbd6b4e3ef3c6e9939578e455f9ec39e6d06

parser: Only reject non-word tokens in case pattern
Allow keywords to be used as case patterns and only reject non-word
tokens.

Reported-by: Nicolas Cavallari <Nicolas.Cavallari@green-communications.fr>
Fixes: e8c0324af64c ("parser: Catch bogus case patterns")
Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

src/parser.c
@@ -442,7 +442,7 @@ next_case:
 			cp->type = NCLIST;
 			app = &cp->nclist.pattern;
 			for (;;) {
-				if (lasttoken != TWORD)
+				if (lasttoken < TWORD)
 					synexpect(TWORD);
 				*app = ap = (union node *)stalloc(sizeof (struct narg));
 				ap->type = NARG;